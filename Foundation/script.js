dea529b0daf1d91bb98fa567f715074d917bd1d7
Olexiy Talalay <49188915+TIIvnderg0D@users.noreply.github.com>
2019-09-25 23:13:59 +0300

    25.09 new

--- Foundation/script.js
+++ Foundation/script.js
@@ -1,14 +1,32 @@
-const menuText = document.querySelector('.submenu-rev');
-const menuItem = document.querySelector('.revealed');
+const menuItem1 = document.getElementById('nav-services');
+const menuItem2 = document.getElementById('nav-works');
+const menuItem3 = document.getElementById('nav-news');
+const menuItem4 = document.getElementById('nav-shop');
+const menuText1 = menuItem1.querySelector('.submenu');
+const menuText2 = menuItem2.querySelector('.submenu');
+const menuText3 = menuItem3.querySelector('.submenu');
+const menuText4 = menuItem4.querySelector('.submenu');
+const nav = document.querySelector('.nav');
+const closeNav = document.getElementById('nav-close');
 
-/*function subMenu() {
-    menuItem.classList.remove('revealed');
-    menuItem.classList.add('hidden');
-    menuText.classList.remove('submenu-rev');
-    menuText.classList.add('submenu');
+
+
+menuText1.onclick = function() {
+    menuItem1.classList.toggle('open');
 }
-menuText.addEventListener('click', subMenu);*/
 
-menuText.onclick = function() {
-    menuItem.classList.toggle('open');
+menuText2.onclick = function() {
+    menuItem2.classList.toggle('open');
+}
+
+menuText3.onclick = function() {
+    menuItem3.classList.toggle('open');
+}
+
+menuText4.onclick = function() {
+    menuItem4.classList.toggle('open');
+}
+
+closeNav.onclick = function() {
+    nav.classList.add('close');
 }
